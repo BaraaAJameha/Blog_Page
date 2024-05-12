0bdb3396f2c9327d80d4e41236aad6c0e82aa926
BaraaAJameha <ajamehabaraa@gmail.com>
2024-05-12 14:05:28 +0300

    tab switching using JavaScript

---
+++ script.js
@@ -0,0 +1,23 @@
+
+/**function to switch to dark mode */
+function myFunction() {
+  let element = document.body;
+  element.classList.toggle("dark");
+}
+
+/*________________________________________________________*/
+
+/* function to Implement tab switching */
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
